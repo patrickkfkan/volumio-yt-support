70225cd7624e2def05225085c620d7195f87ef68
patrickkfkan <patrickkfkan@gmail.com>
2026-03-10 22:02:10 +0800

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/innertube/Constants';
 export * from './lib/innertube/Factory';
 export * from './lib/innertube/Wrapper';
-export type { PotFnResult, EvalFnResult } from './lib/innertube/Server';
+export type { EvalFnResult } from './lib/innertube/Server';
+export type { PoTokenData } from './lib/innertube/PoToken';
 export * from './lib/volumio/AutoplayManager';
